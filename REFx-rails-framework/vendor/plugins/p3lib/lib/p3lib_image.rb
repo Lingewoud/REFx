d63923c7f219d0d347533b53677587362d29b985
Gabriël Ramaker <gabriel@lingewoud.nl>
2013-04-23 11:51:54 +0200

    p3lib image fix for spaces in filenames

--- REFx-rails-framework/vendor/plugins/p3lib/lib/p3lib_image.rb
+++ REFx-rails-framework/vendor/plugins/p3lib/lib/p3lib_image.rb
@@ -9,21 +9,21 @@ class P3libImage
     def getXYRatio(original_size,newWidth, newHeight)
         return newWidth.to_f / original_size.width.to_f, newHeight.to_f / original_size.height.to_f
     end
-
+    
     def self.resizeBitmap(img,pixWidth,pixHeight)
-        cmd = "#{RAILS_ROOT}/../p3imgutils/p3scale -w#{pixWidth} -h#{pixHeight} -i #{img} -o #{img}"
+        cmd = "#{RAILS_ROOT}/../p3imgutils/p3scale -w#{pixWidth} -h#{pixHeight} -i '#{img}' -o '#{img}'"
         #P3libLogger::log("resizing cmd:",cmd)
         system(cmd)
     end
 
     def self.resizeBitmapByWidth(img,pixWidth)
-        cmd = "#{RAILS_ROOT}/../p3imgutils/p3scale -w#{pixWidth} -h0 -i #{img} -o #{img}"
+        cmd = "#{RAILS_ROOT}/../p3imgutils/p3scale -w#{pixWidth} -h0 -i '#{img}' -o '#{img}'"
         #P3libLogger::log("resizing cmd:",cmd)
         system(cmd)
     end
 
     def self.trimAlphaFromImage(inImage,outImage)
-        cmd = "#{RAILS_ROOT}/../p3imgutils/p3trimalpha -i #{inImage} -o #{outImage}"
+        cmd = "#{RAILS_ROOT}/../p3imgutils/p3trimalpha -i '#{inImage}' -o '#{outImage}'"
 
         #P3libLogger::log("trimming alpha to :",outImage)
         #P3libLogger::log("trimming alpha using :",cmd)
@@ -32,10 +32,9 @@ class P3libImage
 
     #TODO TEST
     def self.convertImgToFiletype(inImage,outImage,type)
-        cmd = "#{RAILS_ROOT}/../p3imgutils/p3convfiletype -i #{inImage} -o #{outImage} -t #{type}"
+        cmd = "#{RAILS_ROOT}/../p3imgutils/p3convfiletype -i '#{inImage}' -o '#{outImage}' -t #{type}"
         #P3libLogger::log("trimming alpha to :",outImage)
         system(cmd)
-end
+    end
 
 end
-
