65b82eb39c723aaa7b24d4ab80977be0cb729836
Pim Snel <pim@lingewoud.nl>
2013-08-28 14:13:00 +0200

    make logging smarter, implement debug preference

--- REFx-rails-framework/vendor/plugins/p3lib/lib/p3lib_logger.rb
+++ REFx-rails-framework/vendor/plugins/p3lib/lib/p3lib_logger.rb
@@ -1,30 +1,23 @@
 require 'logger'
 
-class  P3libLogger
-	class << self
-		def log(key, val='', type='info')
-			if(@logger.nil?) then
-				@logger 		= Logger.new("#{RAILS_ROOT}/log/pas3.log")
-				@logger.level 	= Logger::DEBUG
-			end
-
-			case type
-			when 'info'
-				@logger.info Time.now.strftime("%b-%d-%Y %H:%M") +' INFO - '+ key+ ': '+ val.to_s
-			when 'error'
-
-				@logger.error Time.now.strftime("%b-%d-%Y %H:%M") +' ERROR - '+ key+ ': '+ val.to_s
-                #self.mail('Error:'+key,Time.now.to_s+' - '+ key+ ': '+ val)
-
-			when 'warn'
-				@logger.warn Time.now.strftime("%b-%d-%Y %H:%M") +' WARNING - '+ key+ ': '+ val.to_s
-			when 'fatal'
-				@logger.fatal Time.now.strftime("%b-%d-%Y %H:%M") +' FATAL - '+ key+ ': '+ val.to_s
-			when 'unknown'
-				@logger.unknown Time.now.strftime("%b-%d-%Y %H:%M") +' UNKNOWN - '+ key+ ': '+ val.to_s
-			when 'debug'
-				@logger.debug Time.now.strftime("%b-%d-%Y %H:%M") +' DEBUG - '+ key+ ': '+ val.to_s
-			end
-		end
-	end
+class P3libLogger
+    class << self
+    
+        # we use the types info, error, debug, warning
+        def log(key, val='', type='info')
+            
+            if(@logger.nil?) then
+                @logger 		= Logger.new("#{RAILS_ROOT}/log/pas3.log")
+                @logger.level   = Logger::DEBUG
+            end
+            
+            if(!$debug.nil? && type=='debug')
+                logstring = "#{type.upcase} - #{key}#{val==''?'':': '+val}"
+                @logger.info Time.now.strftime("%b-%d-%Y %H:%M") +' '+ logstring
+            elsif(type!='debug')
+                logstring = "#{type.upcase} - #{key}#{val==''?'':': '+val}"
+                @logger.info Time.now.strftime("%b-%d-%Y %H:%M") +' '+ logstring
+            end
+        end
+    end
 end
